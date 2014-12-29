46e62d49e6edc810be6d74ac7f966898fc1e62ce
Mark Cutler <markjcutler@gmail.com>
2014-12-29 13:58:50 -0500

    example dialog box works, but menu got messed up

--- publications.php
+++ publications.php
@@ -113,6 +113,34 @@
     <h1>Efficient Reinforcement Learning for Robots using Informative Simulated Priors</h1>
     <p><b>Mark Cutler</b>, Jonathan P. How<br>
       IEEE International Conference on Robotics and Automation, May 2015 (submitted), Seattle WA</p>
+    <div class="content">
+      <header class="codrops-header">
+        <div class="button-wrap">
+          <button data-dialog="somedialog" class="trigger">Bibtex</button>
+        </div>
+      </header>
+      <div id="somedialog" class="dialog">
+        <div class="dialog__overlay"></div>
+        <div class="dialog__content">
+          <pre>
+ @INPROCEEDINGS{Michini11_ICRA,
+  author = {Michini, B. and Redding, J. and Ure, N. K. and Cutler, M. and How,
+	J. P.},
+  title = {Design and Flight Testing of an Autonomous Variable-Pitch Quadrotor},
+  booktitle = {IEEE International Conference on Robotics and Automation (ICRA)},
+  year = {2011},
+  pages = {2978--2979},
+  month = {May},
+  organization = {IEEE},
+  url = {http://acl.mit.edu/papers/Michini11_ICRA.pdf}
+}</pre>
+          <div>
+            <button class="action" data-dialog-close>Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /content --> 
   </div>
   <div class="mix conference" data-myorder="2015">
     <h1>Decoupled Multiagent Path Planning via Incremental Sequential Convex Programming</h1>
@@ -131,38 +159,6 @@
     </a>
     <p>Bernard Michini, <b>Mark Cutler</b>, Jonathan P. How<br>
       IEEE International Conference on Robotics and Automation, May 2013, Karlsruhe Germany, pp. 303-308</p>
-    <div class="container">
-      <div class="content">
-        <header class="codrops-header">
-          <div class="button-wrap">
-            <button data-dialog="somedialog" class="trigger">Open Dialog</button>
-          </div>
-        </header>
-        <div id="somedialog" class="dialog">
-          <div class="dialog__overlay"></div>
-          <div class="dialog__content">
-            <pre>
- @INPROCEEDINGS{Michini11_ICRA,
-  author = {Michini, B. and Redding, J. and Ure, N. K. and Cutler, M. and How,
-	J. P.},
-  title = {Design and Flight Testing of an Autonomous Variable-Pitch Quadrotor},
-  booktitle = {IEEE International Conference on Robotics and Automation (ICRA)},
-  year = {2011},
-  pages = {2978--2979},
-  month = {May},
-  organization = {IEEE},
-  url = {http://acl.mit.edu/papers/Michini11_ICRA.pdf}
-}</pre>
-            <div>
-              <button class="action" data-dialog-close>Close</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- /content --> 
-    </div>
-    <!-- /container --> 
-    
   </div>
   <div class="mix conference" data-myorder="2011"> <a href="http://acl.mit.edu/papers/Michini11_ICRA.pdf"  target="_blank">
     <h1>Design and Flight Testing of an Autonomous Variable-Pitch Quadrotor</h1>
