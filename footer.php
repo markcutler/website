af5a0ae4d6006c65ae7ffcb1c6fce07db45184d9
Mark Cutler <markjcutler@gmail.com>
2015-03-03 17:18:31 -0500

    adding cv

--- footer.php
+++ footer.php
@@ -5,8 +5,8 @@
   </header>
   <footer>
     <ul class="icons">
-      
-      <!--li><a href="mailto:markjcutler@gmail.com" class="icon fa-envelope-square fa-4x"><span class="label">Email</span></a></li--> 
+
+      <!--li><a href="mailto:markjcutler@gmail.com" class="icon fa-envelope-square fa-4x"><span class="label">Email</span></a></li-->
       <!-- javascript trick to "hide" my email from spambots -->
       <li><script language="JavaScript">
 						var username = "markjcutler";
@@ -16,6 +16,7 @@
 						</script> class="icon fa-envelope-square fa-4x" target="_blank"><span class="label">Email</span></a></li>
       <li><a href="https://www.linkedin.com/pub/mark-cutler/23/939/7a8" class="icon fa-linkedin-square fa-4x" target="_blank"><span class="label">LinkedIn</span></a></li>
       <li><a href="https://bitbucket.org/markjcutler/mark-cutler-overview" class="icon fa-bitbucket-square fa-4x" target="_blank"><span class="label">BitBucket</span></a></li>
+      <!--li><a href="https://scholar.google.com/citations?user=OG-5aqEAAAAJ&hl=en" class="icon fa-bitbucket-square fa-4x" target="_blank"><span class="label">BitBucket</span></a></li-->
       <li><a href="https://www.flickr.com/photos/55516219@N04/" class="icon fa-flickr fa-4x" target="_blank"><span class="label">Flickr</span></a></li>
     </ul>
   </footer>
@@ -24,7 +25,7 @@
 <!-- Footer -->
 <footer id="footer">
   <ul id="Publications" class="copyright">
-    <li>&copy; Mark Cutler <script>document.write(new Date().getFullYear())</script></li> 
+    <li>&copy; Mark Cutler <script>document.write(new Date().getFullYear())</script></li>
   <li><button data-dialog="attributions" class="button link">Attributions</button></li>
     <!--li>Template inspired by <a href="http://html5up.net">HTML5 UP</a></li-->
   </ul>
