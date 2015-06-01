0e2f5a10c839260db5ed07fcb8031069259fb2c5
Mark Cutler <markjcutler@gmail.com>
2015-06-01 11:11:57 -0400

    wip

--- publications/Cutler15_ICRA.php
+++ publications/Cutler15_ICRA.php
@@ -6,11 +6,11 @@
 <i class="fa fa-square-o fa-stack-2x"></i>
 <i class="fa fa-quote-left fa-stack-1x"></i>
 </span></button>
-   <a href="http://markjcutler.com/papers/Cutler15_ICRA.pdf"  target="_blank" class="button icon"><span class="fa-stack fa-lg">
-<i class="fa fa-square-o fa-stack-2x"></i>
-<i class="fa fa-quote-left fa-stack-1x"></i>
-</span></button>
    <a href="http://markjcutler.com/papers/Cutler15_ICRA_additional.pdf"  target="_blank" class="button icon"><span class="fa-stack fa-lg">
+<i class="fa fa-square-o fa-stack-2x"></i>
+<i class="fa fa-cloud-download fa-stack-1x"></i>
+</span></button>
+   <a href="http://markjcutler.com/papers/Cutler15_ICRA.pdf"  target="_blank" class="button icon"><span class="fa-stack fa-lg">
 <i class="fa fa-square-o fa-stack-2x"></i>
 <i class="fa fa-cloud-download fa-stack-1x"></i>
 </span></a>
