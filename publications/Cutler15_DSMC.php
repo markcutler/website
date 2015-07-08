5050fe290c7742ea4e5d672755bb14e63e20223c
Mark Cutler <markjcutler@gmail.com>
2015-07-08 10:01:03 -0400

    updating publications

--- publications/Cutler15_DSMC.php
+++ publications/Cutler15_DSMC.php
@@ -1,7 +1,7 @@
 <div class="mix journal" data-myorder="2015"> 
 <div class="title">Analysis and Control of a Variable-Pitch Quadrotor for Agile Flight</div>
 <div class="authors"><b>Mark Cutler</b>, Jonathan P. How</div>
-<div class="location_info">ASME Journal of Dynamic Systems, Measurement and Control, October 2015, vol. 137, num. 10, pp. 101002-1 - 101002-14
+<div class="location_info">ASME Journal of Dynamic Systems, Measurement and Control, October 2015, vol. 137, num. 10, pp. 1-14
   <button data-dialog="Cutler15_DSMC_bib" class="button icon"><span class="fa-stack fa-lg">
 <i class="fa fa-square-o fa-stack-2x"></i>
 <i class="fa fa-quote-left fa-stack-1x"></i>
