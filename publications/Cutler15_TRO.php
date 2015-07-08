e157c6008861a7a1d1bf3085a91ca926f6f1b75f
Mark Cutler <markjcutler@gmail.com>
2015-07-08 10:00:24 -0400

    updating publications

--- publications/Cutler15_TRO.php
+++ publications/Cutler15_TRO.php
@@ -1,7 +1,7 @@
 <div class="mix journal" data-myorder="2015"> 
 <div class="title">Real-World Reinforcement Learning via Multifidelity Simulators</div>
 <div class="authors"><b>Mark Cutler</b>, Thomas J. Walsh, Jonathan P. How</div>
-<div class="location_info">IEEE Transactions on Robotics, June 2015
+<div class="location_info">IEEE Transactions on Robotics, June 2015, vol. 31, num. 3, pp. 655-671
   <button data-dialog="Cutler15_TRO_bib" class="button icon"><span class="fa-stack fa-lg">
 <i class="fa fa-square-o fa-stack-2x"></i>
 <i class="fa fa-quote-left fa-stack-1x"></i>
